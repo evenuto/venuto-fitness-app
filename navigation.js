7067f814638eb2f98d4aa40073bf69d5724998dc
Emily Venuto <emily.venuto@gmail.com>
2017-09-15 12:42:51 -0400

    add the nav bar to each static page and update the login format

--- navigation.js
+++ navigation.js
@@ -1,13 +1,5 @@
 $(function(){
     
-    $("#header-placeholder").load("_header.html", function(){
-        $(".nav-link").removeClass("active");
-        $(".nav-link[href=signup.html]").addClass("active");
-        //placing this code inside this function allows for proper loading
-    });
-     
-    $(".btn-primary").click(function(){
-        $(this).closest(".col-md-4").css({ 'max-width': 'none'}).animate({ 'flex-basis' : '100%' })
-    })
+    $("#header-placeholder").load("_header.html");
     
     });
