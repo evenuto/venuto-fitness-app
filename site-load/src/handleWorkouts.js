a951912cf924050fd80639b6d6ddf17ec07d8926
Emily Venuto <emily.venuto@gmail.com>
2017-10-26 21:58:58 -0400

    fix cards in workout-track.html

--- site-load/src/handleWorkouts.js
+++ site-load/src/handleWorkouts.js
@@ -1,13 +1,29 @@
 var workoutArray = Array();
 $(document).ready(function() {
     $("#submit").click(function(){
-        if(document.getElementById("workout").value != "" && document.getElementById("duration").value != "")
+        if(document.getElementById("cardio").value != "" && document.getElementById("duration").value != "")
         {
-            workoutArray.push(document.getElementById("workout").value + " for " + document.getElementById("duration").value + "<br>");
+            workoutArray.push(document.getElementById("cardio").value + " for " 
+                + document.getElementById("duration").value + " minutes" + "<br>");
             $("#output").append('<span>' + workoutArray[workoutArray.length - 1] + '</span>');
-            workout.value = "";
+            cardio.value = "";
             duration.value = "";
         }
     });
-   
 });
+
+$(document).ready(function() {
+        $("#submit2").click(function(){
+            if(document.getElementById("lift").value != "" && document.getElementById("weight").value != "" && document.getElementById("set").value != "")
+            {
+                workoutArray.push(document.getElementById("lift").value + " at " 
+                    + document.getElementById("weight").value + " for " +  + document.getElementById("set").value + "<br>");
+                $("#output").append('<span>' + workoutArray[workoutArray.length - 1] + '</span>');
+                lift.value = "";
+                weight.value = "";
+                set.value = "";
+
+            }
+        });
+    });
+   
